e0e8901e1f6ff7b2980ef27a43208adb3286de78
Luke Patton <lpatton@dosomething.org>
2019-05-02 13:31:08 -0400

    Almost finish two cards, Leaky and Starburst

--- assets/builder.js
+++ assets/builder.js
@@ -8,11 +8,18 @@ canvas.height = 500;
 var width = canvas.width;
 var height = canvas.height;
 
-boxed();
+leaky();
 
-function boxed() {
-  // Colors
-  var yellow = '#FEAC00';
+canvas.addEventListener('click', function() {
+  leaky();
+});
+
+function leaky() {
+  // Vars
+  var margin = 10;
+  var radius = 0.03;
+  var targets = [];
+  var points = createGrid();
   var colors = [
     '#FEAC00',
     '#FF5630',
@@ -27,34 +34,51 @@ function boxed() {
   ];
 
   // Backgrounds
-  context.fillStyle = yellow;
+  context.fillStyle = '#396C7D';
   context.fillRect(0, 0, width, height);
 
-  // Double loop to draw boxes
-  for (let i = 0; i < width; ) {
-    let boxWidth = rangeFloor(10, 50);
-
-    for (let j = 0; j < height; ) {
-      let boxHeight = rangeFloor(10, 100);
-
-      // Randomly add gradients
-      if (Math.random() > 0.7) {
-        let grd = context.createLinearGradient(i, j, i, j + boxHeight);
-        grd.addColorStop(0, pick(colors));
-        grd.addColorStop(1, pick(colors));
-        context.fillStyle = grd;
-      } else {
-        context.fillStyle = pick(colors);
-      }
-
-      // Draw the boxes
-      context.fillRect(i, j, boxWidth, boxHeight);
-
-      j = j + boxHeight;
-    }
-    i = i + boxWidth;
+  for (let i = 0; i < 100; i++) {
+    targets.push(pick(points));
   }
 
+  targets.forEach(points => {
+    const u = points[0];
+    const v = points[1];
+
+    const x = lerp(margin, width - margin, u);
+    const y = lerp(margin, height - margin, v);
+
+    const color = pick(colors);
+
+    context.beginPath();
+    context.moveTo(width / 2, height / 2);
+    context.lineTo(x, y);
+    context.lineWidth = (radius * width) / 8;
+    context.strokeStyle = color;
+    context.stroke();
+
+    context.beginPath();
+    context.arc(x, y, (radius * width) / 4, 0, Math.PI * 2, false);
+    context.fillStyle = color;
+    context.fill();
+  });
+
+  function createGrid() {
+    const points = [];
+    const count = 20;
+
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+
+        points.push([u, v]);
+      }
+    }
+    return points;
+  }
+
+  // Math stuff
   function rangeFloor(min, max) {
     // Return a random whole number between min and max
     return Math.floor(Math.random() * (max - min) + min);
@@ -64,5 +88,9 @@ function boxed() {
     // Pick a random item out of an array
     if (array.length === 0) return undefined;
     return array[rangeFloor(0, array.length)];
+  }
+
+  function lerp(min, max, t) {
+    return min * (1 - t) + max * t;
   }
 }
---
+++ assets/cards/leaky.js
@@ -0,0 +1,54 @@
+// Builder
+var canvas = document.getElementById('card-canvas');
+var context = canvas.getContext('2d');
+
+canvas.width = 300;
+canvas.height = 500;
+
+var width = canvas.width;
+var height = canvas.height;
+
+leaky();
+
+canvas.addEventListener('click', function() {
+  leaky();
+});
+
+function leaky() {
+  // Backgrounds
+  context.fillStyle = '#0B1961';
+  context.fillRect(0, 0, width, height);
+  context.globalCompositeOperation = 'lighter';
+
+  var colors = ['#ED9900', '#DF2C0F', '#744491', '#004F80', '#FFD0BF'];
+
+  // Draw a group of lines
+  for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 6; j++) {
+      console.log('Drawing at ' + j * 50 + ' and ' + i * 50);
+      centerRect(
+        j * 50,
+        i * 50,
+        rangeFloor(10, 100),
+        rangeFloor(10, 100),
+        pick(colors),
+      );
+    }
+  }
+
+  function centerRect(x, y, rWidth, rHeight, color) {
+    context.fillStyle = color;
+    context.fillRect(x, y, rWidth, rHeight);
+  }
+
+  function rangeFloor(min, max) {
+    // Return a random whole number between min and max
+    return Math.floor(Math.random() * (max - min) + min);
+  }
+
+  function pick(array) {
+    // Pick a random item out of an array
+    if (array.length === 0) return undefined;
+    return array[rangeFloor(0, array.length)];
+  }
+}
