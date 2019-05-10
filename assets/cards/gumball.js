0d9385ebdfedad39f432d32a201c6314df0182ea
Luke Patton <lpatton@dosomething.org>
2019-05-10 11:13:08 -0400

    Added gumball, slotted blur away for a rainy day

--- assets/cards/gumball.js
+++ assets/cards/gumball.js
@@ -1,5 +1,5 @@
-// Builder
-var canvas = document.getElementById('card-canvas');
+// Gumball
+var canvas = document.getElementById('gumball');
 var context = canvas.getContext('2d');
 
 canvas.width = 300;
