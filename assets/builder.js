2b63064163eda69f0cc2298d0b1968b940f58ce2
Luke Patton <lpatton@dosomething.org>
2019-06-05 16:17:37 -0400

    Working on Holy

--- assets/builder.js
+++ assets/builder.js
@@ -8,22 +8,77 @@ canvas.height = 500;
 var width = canvas.width;
 var height = canvas.height;
 
-blur();
+holy();
 
-function blur() {
-  context.fillStyle = '#ff4b44';
+function holy() {
+  // Vars
+  var margin = 10;
+  var radius = 0.03;
+  var targets = [];
+  var points = createGrid();
+
+  // Backgrounds
+  context.fillStyle = 'cadetblue';
   context.fillRect(0, 0, width, height);
 
-  context.arc(width / 2, height / 2 + 50, 70, 0, Math.PI, false);
-  context.fillStyle = 'rgb(255, 242, 109';
-  context.fill();
+  // Pick random points
+  // for (let i = 0; i < 100; i++) {
+  //   targets.push(pick(points));
+  // }
 
-  for (let i = 0; i < 60; i++) {
+  context.globalCompositeOperation = 'destination-out';
+
+  points.forEach(points => {
+    const u = points[0];
+    const v = points[1];
+
+    const x = lerp(margin, width - margin, u);
+    const y = lerp(margin, height - margin, v);
+
+    const color = 'rgba(0,0,0,1.0)';
+    context.fillStyle = color;
+
+    // context.beginPath();
+    // context.arc(x, y, (radius * width) / 2, 0, Math.PI * 2, false);
+    drawCircle(x, y, 5);
+  });
+
+  context.globalCompositeOperation = 'destination-over';
+
+  let grd = context.createLinearGradient(0, width / 2, height, width / 2);
+  grd.addColorStop(0.0, 'honeydew');
+  grd.addColorStop(1, 'aqua');
+
+  context.fillStyle = grd;
+  context.fillRect(0, 0, width, height);
+
+  function drawCircle(x, y, radius) {
+    context.save();
+    context.translate(x, y);
     context.beginPath();
-    context.filter = `blur(${1 + i * 5}px)`;
-    context.arc(width / 2, height / 2 + 50 - i * 6, 70, 0, Math.PI * 2, false);
-    context.fillStyle = `rgba(255, 242, 109, ${1 - i * 0.05})`;
-
+    context.arc(0, 0, radius, 0, 2 * Math.PI, false);
+    context.lineWidth = 3;
     context.fill();
+    context.restore();
+  }
+
+  function createGrid() {
+    const points = [];
+    const count = 20;
+
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+
+        points.push([u, v]);
+      }
+    }
+    return points;
+  }
+
+  // Math stuff
+  function lerp(min, max, t) {
+    return min * (1 - t) + max * t;
   }
 }
