a5d5842ec1a370b861c4757f266e71b6a049afe5
Luke Patton <lpatton@dosomething.org>
2019-06-27 13:12:10 -0400

    Did some bona fide math to make Perfect Pitch

--- assets/builder.js
+++ assets/builder.js
@@ -1,4 +1,4 @@
-// Shortcake
+// Builder
 var canvas = document.getElementById('card-canvas');
 var context = canvas.getContext('2d');
 
@@ -8,46 +8,92 @@ canvas.height = 500;
 var width = canvas.width;
 var height = canvas.height;
 
-shortcake();
+strange();
 
-function shortcake() {
-  var colors = [
-    '#FF4748',
-    '#FF6C5B',
-    '#FF9070',
-    '#FFAF86',
-    '#FFCA9D',
-    '#FFDFB4',
-    '#FFF0CC',
-  ];
-
+function strange() {
   // Backgrounds
-  context.fillStyle = '#FFCA96';
+  context.fillStyle = '#011628';
   context.fillRect(0, 0, width, height);
 
-  context.fillStyle = '#FFFFFF';
-  var circleNumber = 500;
-  var phi = (Math.sqrt(5) + 1) / 2 - 1;
-  var goldenAngle = phi * 2 * Math.PI;
-  var outerRadius = width * 0.95;
-  var innerRadius = 2;
+  context.translate(width / 2, height / 2);
 
-  var cx = width / 2;
-  var cy = height / 2;
+  const circlePoints = createCirclePoints(300, 36);
 
-  for (var i = 1; i <= circleNumber; ++i) {
-    var ratio = i / circleNumber;
-    var angle = i * goldenAngle;
+  // circlePoints.forEach(point => {
+  //   const x = point[0];
+  //   const y = point[1];
 
-    // Change the radius of the "outer" circle with each loop
-    var spiralRadius = ratio * outerRadius;
-    var x = cx + Math.cos(angle) * spiralRadius;
-    var y = cy + Math.sin(angle) * spiralRadius;
+  //   context.beginPath();
+  //   context.arc(x, y, 3, 0, Math.PI * 2, false);
+  //   context.fillStyle = '#7D6B7D';
+  //   context.fill();
+  // });
 
-    // draw tiny circle at x,y
+  // Change colors and do it again, 15 points around
+  var innerGradient = context.createRadialGradient(0, 0, 30, 0, 0, 300);
+  innerGradient.addColorStop(0.1, '#FF9EE2');
+  innerGradient.addColorStop(1, '#A59EFF');
+  context.strokeStyle = innerGradient;
+  for (let i = 0; i < circlePoints.length; i++) {
+    drawBetweenPoints(i, i + 15);
+  }
+
+  // Draw a line 11 points around the circle
+  var outerGradient = context.createRadialGradient(0, 0, 30, 0, 0, 200);
+  outerGradient.addColorStop(0.1, '#DFABFF');
+  outerGradient.addColorStop(1, '#AE90E8');
+  context.strokeStyle = outerGradient;
+  for (let i = 0; i < circlePoints.length; i++) {
+    drawBetweenPoints(i, i + 11);
+  }
+
+  function drawBetweenPoints(firstPoint, secondPoint) {
+    let first = firstPoint;
+    let second = secondPoint;
+    if (second > circlePoints.length - 1) {
+      second = second - circlePoints.length;
+      console.log(second);
+    }
+
     context.beginPath();
-    context.arc(x, y, innerRadius + i * 0.015, 0, 2 * Math.PI, false);
-    context.fillStyle = colors[i % colors.length];
-    context.fill();
+    context.moveTo(circlePoints[first][0], circlePoints[first][1]);
+    context.lineTo(circlePoints[second][0], circlePoints[second][1]);
+    context.stroke();
+  }
+
+  // Create an array of n points around a circle
+  function createCirclePoints(radius, number) {
+    var points = [];
+    for (let i = 0; i < number; i++) {
+      // Calculate the position of each point
+      // The angle is i * (360 / number)
+      let angle = i * (360 / number);
+      let x = radius * Math.cos((-angle * Math.PI) / 180);
+      let y = radius * Math.sin((-angle * Math.PI) / 180);
+
+      // Add point to array
+      points.push([x, y]);
+    }
+    console.log(points);
+    return points;
   }
 }
+
+// PURPLE PINK
+// Change colors and do it again, 15 points around
+//  var innerGradient = context.createRadialGradient(0, 0, 30, 0, 0, 300);
+//  innerGradient.addColorStop(0.1, '#FF9EE2');
+//  innerGradient.addColorStop(1, '#A59EFF');
+//  context.strokeStyle = innerGradient;
+//  for (let i = 0; i < circlePoints.length; i++) {
+//    drawBetweenPoints(i, i + 15);
+//  }
+
+//  // Draw a line 11 points around the circle
+//  var outerGradient = context.createRadialGradient(0, 0, 30, 0, 0, 200);
+//  outerGradient.addColorStop(0.1, '#DFABFF');
+//  outerGradient.addColorStop(1, '#AE90E8');
+//  context.strokeStyle = outerGradient;
+//  for (let i = 0; i < circlePoints.length; i++) {
+//    drawBetweenPoints(i, i + 11);
+//  }
