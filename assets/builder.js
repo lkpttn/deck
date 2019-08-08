d448acb7b13b9118c42296fcf0480f0f25c8e5e0
Luke <lkpttn@gmail.com>
2019-08-08 14:06:45 -0400

    New idea, Zone

--- assets/builder.js
+++ assets/builder.js
@@ -8,57 +8,48 @@ canvas.height = 500;
 var width = canvas.width;
 var height = canvas.height;
 
-rose();
+operator();
 
-function rose() {
+function operator() {
+  // Vars
+  var grd = context.createLinearGradient(0, 0, width, height);
+  grd.addColorStop(0.0, '#E47D7F');
+  grd.addColorStop(0.5, '#6F81D6');
+  grd.addColorStop(1.0, '#7AAEAD');
+
+  var grd2 = context.createLinearGradient(0, 0, width, height);
+  grd2.addColorStop(0.0, '#609096');
+  grd2.addColorStop(0.5, '#FAF9FB');
+  grd2.addColorStop(1.0, '#FCD4A9');
+
   // Backgrounds
-  context.fillStyle = '#0D1E34';
+  context.fillStyle = '#17252D';
   context.fillRect(0, 0, width, height);
 
-  context.translate(width / 2, height / 2);
+  drawRings(60, grd, 18);
+  drawRings(50, grd2, 3);
 
-  const circlePoints = createCirclePoints(140, 8);
-  var colors = [
-    '#FDAC00', // Orange
-    '#EEB720', // Yellow
-    '#55A56C', // Green
-    '#F47579', // Pink
-  ];
+  function drawRings(offset, gradient, lineWidth) {
+    // Existing content is kept where it doesn't overlap with the shape
+    // aka everything else we draw will punch a hole in the background
+    context.globalCompositeOperation = 'destination-out';
+    context.translate(width / 2, height / 2);
 
-  for (let i = 0; i < circlePoints.length; i++) {
-    context.strokeStyle = colors[i % colors.length];
-    drawToPoints(i, circlePoints);
-  }
-
-  function drawToPoints(point, array) {
-    let origin = array[point];
-
-    context.beginPath();
-    context.moveTo(origin[0], origin[1]);
-    // Draw a line to all points in array
-    for (let j = 0; j < circlePoints.length; j++) {
-      let destination = circlePoints[j];
-      context.lineTo(destination[0], destination[1]);
-      context.moveTo(origin[0], origin[1]);
+    // Draw rings
+    for (let i = 0; i < 15; i++) {
+      context.beginPath();
+      context.arc(0, 0, offset + i * 20, 0, Math.PI * 2, false);
+      context.strokeStyle = 'white';
+      context.lineWidth = lineWidth;
+      context.stroke();
     }
 
-    context.stroke();
-  }
+    // New shapes will be drawn behind the existing content
+    // We use this to draw the gradient behind the layer with holes
+    context.globalCompositeOperation = 'destination-over';
+    context.translate(-width / 2, -height / 2);
 
-  // Create an array of n points around a circle
-  function createCirclePoints(radius, number) {
-    var points = [];
-    for (let i = 0; i < number; i++) {
-      // Calculate the position of each point
-      // The angle is i * (360 / number)
-      let angle = i * (360 / number);
-      let x = radius * Math.cos((-angle * Math.PI) / 180);
-      let y = radius * Math.sin((-angle * Math.PI) / 180);
-
-      // Add point to array
-      points.push([x, y]);
-    }
-    console.log(points);
-    return points;
+    context.fillStyle = gradient;
+    context.fillRect(0, 0, width, height);
   }
 }
