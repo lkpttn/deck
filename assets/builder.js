314e4af52bbd022b6daaa8b240332ec5c22185c4
Luke Patton <lpatton@dosomething.org>
2019-08-23 10:32:45 -0400

    Drastically changed from Berlin, given a new name

--- assets/builder.js
+++ assets/builder.js
@@ -13,58 +13,66 @@ berlin();
 function berlin() {
   // The counts and multiplier determine how many grid points we have
   // and how spaced out they are
-  const countX = 8;
-  const countY = 40;
-  const multiplier = 30;
-  const margin = 40;
+  const countX = 40;
+  const countY = 50;
+  const margin = 0;
 
   // The grid contains nested arrays of coordinates. Each x array
   // has objects in in that contain x and y properties like {x: 5, y: 7}
-  const grid = createGrid();
+  var points = createGrid();
 
-  var symbols = ['—', '▲', '▼', '●'];
+  var symbols = ['▬', '‑', '‒', '—', '‑', '‒', '—'];
 
   var colors = [
-    '#FFB713', // Gold
-    '#5200C5', // Purple
-    '#009F45', // Green
-    '#FF3A5C', // Redish
-    '#4646DF', // Blue
-    '#F44918', // Orange
+    '#F58CFF', // Pink
+    '#FFAEE3', // Pale Pink
+    '#FFD6D5', // Peach
+    '#5983F5', // Blue
+    '#9B6EFF', // Purple
+    '#3ECCE4', // Aqua
   ];
 
   // Backgrounds
-  context.fillStyle = '#000000';
+  context.fillStyle = '#4646DF';
   context.fillRect(0, 0, width, height);
 
-  // Draw a shape at every third point
-  for (let i = 0; i < countX; i++) {
-    for (let j = 0; j < countY; j++) {
-      let point = grid[i][j];
+  points.forEach(points => {
+    // Deconstruct into variables
+    // u and v values are between 0..1
+    var u = points[0];
+    var v = points[1];
 
-      context.save();
-      context.fillStyle = pick(colors);
-      context.font = `24px "Arial"`;
-      context.translate(point.x, point.y);
-      context.fillText(pick(symbols), 0, 0);
-      context.restore();
-    }
-  }
+    // Lerp will distribute the point on our canvas based on
+    // it's value between 0..1
+    var x = lerp(margin, width - margin, u);
+    var y = lerp(margin, height - margin, v);
+
+    // A little bit of rounding to help with subpixel rendering
+    context.save();
+    context.fillStyle = pick(colors);
+    context.font = `36px "Arial"`;
+    context.fillText(pick(symbols), x, y);
+    context.restore();
+  });
 
   // Functions
-  // Nested for loop to create x and y coordinates
   function createGrid() {
-    let points = [];
+    const points = [];
+
+    // Will return a set of points between 0..1
     for (let x = 0; x < countX; x++) {
-      points[x] = [];
       for (let y = 0; y < countY; y++) {
-        points[x][y] = {
-          x: margin + x * multiplier,
-          y: margin + y * multiplier,
-        };
+        const u = countX <= 1 ? 0.5 : x / (countX - 1);
+        const v = countY <= 1 ? 0.5 : y / (countY - 1);
+
+        points.push([u, v]);
       }
     }
     return points;
+  }
+
+  function lerp(min, max, t) {
+    return min * (1 - t) + max * t;
   }
 
   function rangeFloor(min, max) {
