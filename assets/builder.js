f47b74032b21dbd76ef36cf708933a47a23920fa
Luke <lkpttn@gmail.com>
2019-08-25 18:49:20 -0400

    giving up on animation math

--- assets/builder.js
+++ assets/builder.js
@@ -11,15 +11,20 @@ var height = canvas.height;
 chronos();
 
 var step = 0;
-var rotationX = 0;
+var radiusX = 0;
 var countup = true;
+
+var rotation = 0;
+var countupY = true;
 
 function chronos() {
   // Vars
   var rings = 3;
   var now = new Date();
+  var minutes = now.getMinutes();
   var seconds = now.getSeconds();
   var miliseconds = now.getMilliseconds();
+  var coords = ['x: 15, y:15'];
 
   // Backgrounds
   context.save();
@@ -29,49 +34,42 @@ function chronos() {
   // Move to center
   context.translate(width / 2, height / 2);
 
-  let miliMapped = mapRange(miliseconds, 0, 1000, 0, 50);
-
-  if (countup) {
-    rotationX++;
-    if (rotationX >= 50) {
-      countup = false;
-    }
-  } else {
-    rotationX--;
-    if (rotationX <= 0) {
-      countup = true;
-    }
-  }
-
+  // Rings
   context.beginPath();
-  context.ellipse(0, 0, rotationX, 50, 0, 0, Math.PI * 2, false);
+  context.ellipse(0, 0, 40, 150, degreeToRadian(15), 0, Math.PI * 2, false);
   context.stroke();
 
-  // Rings
-  // for (let i = 0; i < rings; i++) {
-  //   context.beginPath();
-  //   context.ellipse(
-  //     0, // x position
-  //     0, // y position
-  //     rotationX + i * 13, // x radius
-  //     50 + i * 13, // y radius
-  //     0, // rotation
-  //     0, // start angle
-  //     2 * Math.PI, // end angle
-  //   );
-  //   context.stroke();
-  // }
+  context.beginPath();
+  context.ellipse(
+    0,
+    0,
+    120,
+    minutes,
+    degreeToRadian(45),
+    0,
+    Math.PI * 2,
+    false,
+  );
+  context.stroke();
 
-  // // Sphere
+  context.beginPath();
+  context.ellipse(0, 0, 90, 85, degreeToRadian(78), 0, Math.PI * 2, false);
+  context.stroke();
+
+  // context.beginPath();
+  // context.ellipse(0, 0, 70, 100, 0, 0, Math.PI * 2, false);
+  // context.stroke();
+
+  // Sphere
   context.beginPath();
   context.fillStyle = '#E95B1C';
   context.arc(0, 0, 30, 0, Math.PI * 2, false);
   context.fill();
 
-  // 3D effect
-  context.beginPath();
-  context.ellipse(0, 0, rotationX, 50, 0, (Math.PI * 3) / 2, Math.PI / 2, true);
-  context.stroke();
+  // // 3D effect
+  // context.beginPath();
+  // context.ellipse(0, 0, 50, 50, 0, (Math.PI * 3) / 2, Math.PI / 2, true);
+  // context.stroke();
 
   window.requestAnimationFrame(chronos);
   context.restore();
@@ -82,21 +80,5 @@ function chronos() {
 
   function mapRange(value, inMin, inMax, outMin, outMax) {
     return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
-  }
-
-  function drawRings(offset, gradient, lineWidth) {
-    // Existing content is kept where it doesn't overlap with the shape
-    // aka everything else we draw will punch a hole in the background
-    context.globalCompositeOperation = 'destination-out';
-    context.translate(width / 2, height / 2);
-
-    // Draw rings
-    for (let i = 0; i < 15; i++) {
-      context.beginPath();
-      context.arc(0, 0, offset + i * 20, 0, Math.PI * 2, false);
-      context.strokeStyle = 'white';
-      context.lineWidth = lineWidth;
-      context.stroke();
-    }
   }
 }
