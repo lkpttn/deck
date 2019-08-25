b94289b929e654e9ac1485972ebd171528c8f522
Luke <lkpttn@gmail.com>
2019-08-25 17:11:44 -0400

    Animation headaches

--- assets/builder.js
+++ assets/builder.js
@@ -8,26 +8,81 @@ canvas.height = 500;
 var width = canvas.width;
 var height = canvas.height;
 
-zoned();
+chronos();
 
-function zoned() {
+var step = 0;
+var rotationX = 0;
+var countup = true;
+
+function chronos() {
   // Vars
-  var grd = context.createLinearGradient(0, 0, width, height);
-  grd.addColorStop(0.0, '#E47D7F');
-  grd.addColorStop(0.5, '#6F81D6');
-  grd.addColorStop(1.0, '#7AAEAD');
-
-  var grd2 = context.createLinearGradient(0, 0, width, height);
-  grd2.addColorStop(0.0, '#609096');
-  grd2.addColorStop(0.5, '#FAF9FB');
-  grd2.addColorStop(1.0, '#FCD4A9');
+  var rings = 3;
+  var now = new Date();
+  var seconds = now.getSeconds();
+  var miliseconds = now.getMilliseconds();
 
   // Backgrounds
+  context.save();
   context.fillStyle = '#fff';
   context.fillRect(0, 0, width, height);
 
-  drawRings(60, grd, 18);
-  drawRings(50, grd2, 3);
+  // Move to center
+  context.translate(width / 2, height / 2);
+
+  let miliMapped = mapRange(miliseconds, 0, 1000, 0, 50);
+
+  if (countup) {
+    rotationX++;
+    if (rotationX >= 50) {
+      countup = false;
+    }
+  } else {
+    rotationX--;
+    if (rotationX <= 0) {
+      countup = true;
+    }
+  }
+
+  context.beginPath();
+  context.ellipse(0, 0, rotationX, 50, 0, 0, Math.PI * 2, false);
+  context.stroke();
+
+  // Rings
+  // for (let i = 0; i < rings; i++) {
+  //   context.beginPath();
+  //   context.ellipse(
+  //     0, // x position
+  //     0, // y position
+  //     rotationX + i * 13, // x radius
+  //     50 + i * 13, // y radius
+  //     0, // rotation
+  //     0, // start angle
+  //     2 * Math.PI, // end angle
+  //   );
+  //   context.stroke();
+  // }
+
+  // // Sphere
+  context.beginPath();
+  context.fillStyle = '#E95B1C';
+  context.arc(0, 0, 30, 0, Math.PI * 2, false);
+  context.fill();
+
+  // 3D effect
+  context.beginPath();
+  context.ellipse(0, 0, rotationX, 50, 0, (Math.PI * 3) / 2, Math.PI / 2, true);
+  context.stroke();
+
+  window.requestAnimationFrame(chronos);
+  context.restore();
+
+  function degreeToRadian(degree) {
+    return (degree * Math.PI) / 180;
+  }
+
+  function mapRange(value, inMin, inMax, outMin, outMax) {
+    return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+  }
 
   function drawRings(offset, gradient, lineWidth) {
     // Existing content is kept where it doesn't overlap with the shape
@@ -43,13 +98,5 @@ function zoned() {
       context.lineWidth = lineWidth;
       context.stroke();
     }
-
-    // New shapes will be drawn behind the existing content
-    // We use this to draw the gradient behind the layer with holes
-    context.globalCompositeOperation = 'destination-over';
-    context.translate(-width / 2, -height / 2);
-
-    context.fillStyle = gradient;
-    context.fillRect(0, 0, width, height);
   }
 }
