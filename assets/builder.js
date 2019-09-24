6ac97a4ceed93d744ca667375a8367956d2930e9
Luke Patton <lpatton@dosomething.org>
2019-09-24 17:27:15 -0400

    More versions Sunset

--- assets/builder.js
+++ assets/builder.js
@@ -15,70 +15,90 @@ function sunset() {
   const margin = 20;
   // The different sizes an individual window can be
   const windowWidths = [20, 36, 50, 73, 120];
-  const windowHeights = [20, 40, 80];
+  const windowHeights = [40, 60, 80];
+  var backgroundColor = '#FFB77C';
   var colors = [
-    '#FFD14E', // Yellow
-    '#FF9C00', // Bright Orange
-    '#EB7420', // Orange
-    '#CA0005', // Bright Red
-    '#A40315', // Dark Red
-    '#034B5E', // Dark Blue
-    '#656597', // Light Purple
-    '#46426C', // Dark Purple
-    '#004135', // Dark Green
-    '#ADA6AD', // Purple Grey
-    '#685A54', // Light Brown
-    '#03060B', // Dark Brown
+    '#002F40', // Dark Blue
+    '#005F73', // Medium Blue
+    '#00ACB9', // Light Blue
+    '#664D5C', // Dull Red
+    '#BC6A60', // Medium Red
+    '#D9A8AF', // Light Red
+    '#EB783C', // Medium Orange
+    '#FFA051', // Light Orange
+    '#FECA55', // Yellow
   ];
-  var backgroundColor = pick(colors);
-  var windowColors = arrayRemove(colors, backgroundColor);
-  console.log(backgroundColor);
-  console.log(windowColors);
 
   // Backgrounds
-  context.fillStyle = backgroundColor;
+  let bgGrd = context.createLinearGradient(0, 0, 0, height);
+  bgGrd.addColorStop(0, backgroundColor);
+  bgGrd.addColorStop(1, shadeColor(backgroundColor, -10));
+  context.fillStyle = bgGrd;
   context.fillRect(0, 0, width, height);
 
-  // Row of windows
-  // Pick a window size, lerp as many as will fit across the width
+  // Double loop to draw windows
+  for (let i = 0; i < height - margin * 2; ) {
+    let boxWidth = pick(windowWidths);
+    let boxHeight = pick(windowHeights);
 
-  var y = margin;
+    for (let j = 0; j < width - margin; ) {
+      let x = margin + j;
+      let y = margin + i;
+      // Draw the boxes
+      if (Math.random() > 0.7) {
+        let grd = context.createLinearGradient(x, y, x, y + boxHeight);
+        grd.addColorStop(0, pick(colors));
+        grd.addColorStop(1, shadeColor(pick(colors), -20));
+        context.fillStyle = grd;
+      } else {
+        context.fillStyle = pick(colors);
+      }
+      context.fillRect(x, y, boxWidth, boxHeight);
+      context.fillStyle = '#03060b';
+      context.fillRect(x, y, boxWidth, 7);
 
-  for (let i = 0; i < 10; i++) {
-    let windowWidth = pick(windowWidths);
-    let windowHeight = pick(windowHeights);
-    for (let j = 0; j < Math.floor(width / windowWidth); j++) {
-      context.fillStyle = pick(windowColors);
-      context.fillRect(
-        margin + j * windowWidth + margin * j,
-        y + margin * i,
-        windowWidth,
-        windowHeight,
-      );
+      j = j + boxWidth + margin;
     }
-
-    if (y + windowHeight < height - margin) {
-      y += windowHeight;
-    } else {
-      console.log('Overflowing');
-    }
+    i = i + boxHeight + margin;
   }
 
   // Functions
+  // Return a random whole number between min and max
   function rangeFloor(min, max) {
-    // Return a random whole number between min and max
     return Math.floor(Math.random() * (max - min) + min);
   }
 
+  // Pick a random item out of an array
   function pick(array) {
-    // Pick a random item out of an array
     if (array.length === 0) return undefined;
     return array[rangeFloor(0, array.length)];
   }
 
+  // Makes a new array, removing a specific value
   function arrayRemove(array, value) {
     return array.filter(function(element) {
       return element != value;
     });
+  }
+
+  // Takes a color and percentage to spit out a new darker or lighter version
+  function shadeColor(color, percent) {
+    color = color.substr(1);
+    var num = parseInt(color, 16),
+      amt = Math.round(2.55 * percent),
+      R = (num >> 16) + amt,
+      G = ((num >> 8) & 0x00ff) + amt,
+      B = (num & 0x0000ff) + amt;
+    return (
+      '#' +
+      (
+        0x1000000 +
+        (R < 255 ? (R < 1 ? 0 : R) : 255) * 0x10000 +
+        (G < 255 ? (G < 1 ? 0 : G) : 255) * 0x100 +
+        (B < 255 ? (B < 1 ? 0 : B) : 255)
+      )
+        .toString(16)
+        .slice(1)
+    );
   }
 }
