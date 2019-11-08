1f4d5297ed7f24ff50d8fc034a6e2b8d3108ba3e
Luke Patton <lpatton@dosomething.org>
2019-11-08 16:30:36 -0500

    Started astro/dream

--- assets/builder.js
+++ assets/builder.js
@@ -12,76 +12,114 @@ context.scale(2, 2);
 var width = canvas.width / 2;
 var height = canvas.height / 2;
 
-skull();
+astro();
 
-function skull() {
+function astro() {
   // Vars
-  var centerX = width / 2;
-  var centerY = height / 2;
-  var cornerRadius = 20;
-
-  // Set faux rounded corners
-  context.lineJoin = 'round';
-  context.lineWidth = cornerRadius;
+  const margin = 30;
+  var countX = 5;
+  var countY = 10;
+  var points = createGrid();
 
   // Backgrounds
-  context.fillStyle = '#FFF';
+  context.fillStyle = '#000';
   context.fillRect(0, 0, width, height);
 
-  // Left eye
-  fillCircle(0, 0, 125, 'black');
+  context.scale(1.3, 1.3);
+  context.translate(-100, 0);
+  context.rotate((Math.PI / 180) * -17);
 
-  // Right eye
-  fillCircle(300, 0, 125, 'black');
+  points.forEach(points => {
+    // Deconstruct into variables
+    // u and v values are between 0..1
+    const { postion, shape } = points;
+    const [u, v] = postion;
 
-  // Nose
-  // cornerRadius = 30;
-  context.beginPath();
-  context.moveTo(centerX - 60, centerY + 35);
-  context.lineTo(centerX, centerY - 60);
-  context.lineTo(centerX + 60, centerY + 35);
-  context.closePath();
-  context.strokeStyle = 'black';
-  context.fillStyle = 'black';
-  context.stroke();
-  context.fill();
+    // Lerp will distribute the point on our canvas based on
+    // it's value between 0..1
+    var x = lerp(margin, width - margin, u);
+    var y = lerp(margin, height - margin, v);
 
-  // Mouth
-  roundedRect(0, 400, 500, 200, 'black');
-  fillCircle(0, 430, 75, 'black');
-  fillCircle(width, 430, 75, 'black');
-  roundedRect(68, 388, 164, 20, 'white');
-  roundedRect(68, 450, 164, 200, 'white');
+    // A little bit of rounding to help with subpixel rendering
+    // drawCircle(Math.round(x), Math.round(y), radius);
+    if (shape === 'Star') {
+      drawStar(x, y, 15, 4, '#FFF');
+    } else if (shape === 'Moon') {
+      drawCrescent(x, y, 15, '#FFF');
+    }
+  });
 
-  // Teeth
-  for (let i = 0; i < 3; i++) {
-    roundedRect(88 + i * 50, 465, 25, 45, 'black');
+  function drawStar(x, y, outer, inner, color) {
+    var angle = Math.PI / 4;
+
+    context.fillStyle = color;
+    context.beginPath();
+
+    for (var i = 0; i < 2 * 4; i++) {
+      var r = i & 1 ? inner : outer;
+      var point_x = x + Math.cos(i * angle) * r;
+      var point_y = y + Math.sin(i * angle) * r;
+
+      if (!i) context.moveTo(point_x, point_y);
+      else context.lineTo(point_x, point_y);
+    }
+
+    context.closePath();
+    context.fill();
   }
 
-  // Overlay
-  context.globalCompositeOperation = 'multiply';
-  context.fillStyle = '#f2f2f2';
-  context.fillRect(centerX, 0, width / 2, height);
-
-  // FUNCTIONS ************
-  function fillCircle(x, y, radius, color) {
+  function drawCrescent(x, y, radius, color) {
+    context.fillStyle = color;
     context.beginPath();
     context.arc(x, y, radius, 0, Math.PI * 2, false);
-    context.fillStyle = color;
+    context.fill();
+
+    context.fillStyle = '#000';
+    context.beginPath();
+    context.arc(
+      x + radius / 2,
+      y - radius / 2.5,
+      radius,
+      0,
+      Math.PI * 2,
+      false,
+    );
     context.fill();
   }
 
-  function roundedRect(x, y, width, height, color) {
-    context.beginPath();
-    context.rect(
-      x + cornerRadius / 2,
-      y + cornerRadius / 2,
-      width - cornerRadius,
-      height - cornerRadius,
-    );
-    context.fillStyle = color;
-    context.strokeStyle = color;
-    context.fill();
-    context.stroke();
+  function createGrid() {
+    const points = [];
+
+    // Will return a set of points between 0..1
+    for (let x = 0; x < countX; x++) {
+      for (let y = 0; y < countY; y++) {
+        const u = countX <= 1 ? 0.5 : x / (countX - 1);
+        const v = countY <= 1 ? 0.5 : y / (countY - 1);
+        let shape;
+        if ((isEven(y) && isEven(x)) || (isOdd(y) && isOdd(x))) {
+          shape = 'Star';
+        } else {
+          shape = 'Moon';
+        }
+        points.push({
+          shape: shape, // Star or moon
+          postion: [u, v],
+        });
+      }
+    }
+    return points;
+  }
+
+  // Math stuff
+  function lerp(min, max, t) {
+    return min * (1 - t) + max * t;
+  }
+
+  function isEven(n) {
+    return n % 2 == 0;
+  }
+
+  function isOdd(n) {
+    return Math.abs(n % 2) == 1;
   }
 }
